17d6e3e9b7500b0d14a785fd5f1a4fe6f557d557
vnnadozie <onyinechianosike@yahoo.co.uk>
2022-12-14 15:38:39 +0000

    Update pharm_sql.sql

--- pharm_sql.sql
+++ pharm_sql.sql
@@ -1,3 +1,5 @@
+--- create table named pharm_data
+
 CREATE TABLE pharm_data(
 	LOCATION VARCHAR,
 	TIME INTEGER,
@@ -8,6 +10,7 @@ CREATE TABLE pharm_data(
 	TOTAL_SPEND DECIMAL	
 );
 
+--- create table named latlongdata
 
 CREATE TABLE latlongdata(
 	LOCATION VARCHAR,
@@ -20,7 +23,9 @@ CREATE TABLE latlongdata(
 SELECT * FROM pharm_data;
 SELECT * FROM latlongdata;
 
+--- inner join latlongdata table to pharm_data table to create one table
 SELECT *
 FROM pharm_data
 INNER JOIN latlongdata
 ON pharm_data.location = latlongdata.location;
+
